c07a4ba0452c10b838852f0931b0c69b2f17378f
LekhaRV <lekha.r.varier@gmail.com>
2025-06-13 15:28:38 +0530

    Change the content of project for better understanding

--- script.js
+++
@@ -1,8 +0,0 @@
-// You can enhance interactivity here (e.g., smooth scroll, active link indicator)
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', () => {
-      document.querySelectorAll('.nav-link').forEach(l => l.classList.remove('active'));
-      link.classList.add('active');
-    });
-  });
-  
